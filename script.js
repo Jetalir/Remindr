db441e336543b03efebc79cfbf20deabaab5c0ea
Jetlir Kuci <jetalir@hotmail.com>
2025-04-21 14:27:18 +0200

    add note button works but not finished

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const taskList = document.getElementById('task-list');
     const radioBtns = document.querySelectorAll('.new input[name="priority"]');
     const note = document.querySelector('.new .note');
+    const addBtn = document.querySelector('.new .addbtn');
 
     // create a new task input
     function createTaskInput(isFirst = false) {
@@ -31,6 +32,37 @@ document.addEventListener('DOMContentLoaded', () => {
   
       return li;
     }
+
+    function createNote(){
+        const note = document.createElement('div');
+        note.className = 'note' + ' ' + document.querySelector('.new input[name="priority"]:checked').value;
+
+        // Add pin to the note
+        const pin = document.createElement('div');
+        pin.className = 'pin'; 
+        note.appendChild(pin);
+
+        
+        const content = document.createElement('div');  
+        content.className = 'note-content';
+        note.appendChild(content);
+
+        const type = document.createElement('h3');
+        type.textContent = document.querySelector('.new .type-input').value; 
+        content.appendChild(type);
+
+        const unorderedList = document.createElement('ul'); 
+        content.appendChild(unorderedList);
+
+        const taskItems = taskList.querySelectorAll('.task-item');
+        taskItems.forEach((taskItem) => {
+            const taskText = taskItem.querySelector('.task-input').value;
+            const li = document.createElement('li');
+            li.textContent = taskText;
+            unorderedList.appendChild(li);
+        });
+        return note;
+    }
   
     // Press Enter to add a new task
     taskList.addEventListener('keydown', (e) => {
@@ -41,7 +73,15 @@ document.addEventListener('DOMContentLoaded', () => {
         newTask.querySelector('.task-input').focus();
       }
     });
-
+    addBtn.addEventListener('click', () => {
+        // Create a new note with the tasks
+        const note = createNote();
+        document.querySelector('.todo').appendChild(note);
+        
+        // Clear the task list after creating the note
+        taskList.innerHTML = '';
+        taskList.appendChild(createTaskInput(true));
+    });
     radioBtns.forEach((radioBtn) => {
       radioBtn.addEventListener('change', () => {
         // Update the note with the selected priority
@@ -49,7 +89,8 @@ document.addEventListener('DOMContentLoaded', () => {
           note.classList.add(radioBtn.value);
       });
     });
-  
+    
+
     // Initialize the first inputbox without a remove button
     taskList.innerHTML = '';
     taskList.appendChild(createTaskInput(true));
